755812865fe337b6daf78158d38db08624331b76
Andrea Pozzetti <andrea@pozzetti.it>
2026-02-11 22:38:06 +0100

    added web directory back

--- __init__.py
+++ __init__.py
@@ -25,4 +25,5 @@ try:
 except Exception:
     pass
 
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+WEB_DIRECTORY = "./web"
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
