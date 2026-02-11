f2a8c5e1064d7a32310417bef09d6d3348dde5d3
Andrea Pozzetti <andrea@pozzetti.it>
2026-02-11 21:37:06 +0100

    everything as process

--- nodes/lib/unirig/src/inference/__init__.py
+++ nodes/lib/unirig/src/inference/__init__.py
@@ -1,1 +1,8 @@
 # UniRig inference module
+
+# Direct preprocessing (bpy as Python module)
+try:
+    from .direct_preprocess import preprocess_mesh
+except ImportError:
+    # bpy not available - fallback to subprocess will be used
+    preprocess_mesh = None
